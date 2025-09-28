16cd12489121e964f9c51a33af5a57f023b9a76c
Admin <rashinkp001@gmail.com>
2025-09-28 16:23:36 +0530

    fixed error midleware issue

--- server/src/middleware/errorMiddleware.ts
+++ server/src/middleware/errorMiddleware.ts
@@ -1,15 +1,19 @@
-import type {  Request, Response } from "express";
-import { HttpStatus } from "../constants/http.status";
-import { ResponseMessages } from "../constants/response.messages";
-import { ErrorCodes } from "../constants/error.codes";
+import type { Request, Response } from "express";
 import { ResponseHelper } from "../utils/response.helper";
+import { ErrorCodes, HttpStatus, ResponseMessages } from "../constants";
 
-export const errorHandler = function (err: Error, req: Request, res: Response) {
+export const errorHandler = function (
+  err: Error,
+  req: Request,
+  res: Response,
+) {
   console.error(err.stack);
+
   const { response } = ResponseHelper.error(
     ResponseMessages.INTERNAL_ERROR,
     ErrorCodes.INTERNAL_ERROR,
     HttpStatus.INTERNAL_SERVER_ERROR
   );
+
   res.status(HttpStatus.INTERNAL_SERVER_ERROR).json(response);
-}
+};
